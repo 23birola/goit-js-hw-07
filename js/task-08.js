514accba08da5c0b6748511d37da5f714be5b9c1
Olga <23birola@gmail.com>
2021-04-24 16:21:06 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,45 @@
-const numberEl = +document.querySelector('input').value;
+// const numberEl = +document.querySelector('input').value;
+// const boxesContainerEl = document.querySelector('#boxes');
+// const getRandom = function (max) {
+//     return Math.floor(Math.random() * max);
+// }
+// const boxes = [];
+// const createBoxes = function (amount) {
+//     let boxHeight = 30;
+//     let boxWidth = 30;
+//     for (let i = 1; i <= amount; i += 1) {
+//         const boxEl = document.createElement('div');
+//         boxEl.style.backgroundColor = `rgb(${getRandom(255)}, ${getRandom(255)}, ${getRandom(255)})`;
+
+//         if (i > 1) {
+//             boxHeight += 10;
+//             boxWidth += 10;
+//         }
+//         boxEl.style.height = `${boxHeight}px`;
+//         boxEl.style.width = `${boxWidth}px`;
+//         boxes.push(boxEl);
+//     }
+//     boxesContainerEl.append(...boxes);
+// }
+
+// const createBtn = document.querySelector('button[data-action="render"]');
+// createBtn.addEventListener('click', createBoxes(numberEl));
+// const clearBtn = document.querySelector('button[data-action="destroy"]');
+// clearBtn.addEventListener('click', () => boxesContainerEl.innerHTML = '');
+
 const boxesContainerEl = document.querySelector('#boxes');
 const getRandom = function (max) {
     return Math.floor(Math.random() * max);
 }
-const boxes = [];
-const createBoxes = function (amount) {
+const createBoxes = function () {
+  console.log('hello')
+  const boxes = [];
+  const numberEl = +document.querySelector('input').value;
     let boxHeight = 30;
     let boxWidth = 30;
-    for (let i = 1; i <= amount; i += 1) {
+    for (let i = 0; i <= numberEl; i += 1) {
         const boxEl = document.createElement('div');
         boxEl.style.backgroundColor = `rgb(${getRandom(255)}, ${getRandom(255)}, ${getRandom(255)})`;
-
         if (i > 1) {
             boxHeight += 10;
             boxWidth += 10;
@@ -19,12 +48,10 @@ const createBoxes = function (amount) {
         boxEl.style.width = `${boxWidth}px`;
         boxes.push(boxEl);
     }
+    console.log(boxes)
     boxesContainerEl.append(...boxes);
 }
-
 const createBtn = document.querySelector('button[data-action="render"]');
-createBtn.addEventListener('click', createBoxes(numberEl));
+createBtn.addEventListener('click', createBoxes);
 const clearBtn = document.querySelector('button[data-action="destroy"]');
 clearBtn.addEventListener('click', () => boxesContainerEl.innerHTML = '');
-
-
